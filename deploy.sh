b79eca0d539ee186d5058fd96d087a27f24f504d
Jack McCracken <jackamc19@gmail.com>
2014-07-08 12:07:32 -0500

    Fixed some bugs with deploy script. Added vim bundle install.

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,13 @@
 command -v zsh >/dev/null 2>&1 || { echo >&2 "This script requires zsh to be installed and on PATH.  Aborting."; exit 1; }
 command -v vim >/dev/null 2>&1 || { echo >&2 "This script requires vim to be installed and on PATH. Aborting."; exit 1; }
 
-git submodule update --init --recursive
+git submodule update --init --recursive;
 
-vim -es -c "BundleInstall"
+for f in .*; 
+do
+if [ "$f" != "." ] && [ "$f" != ".." ]; then
+  ln -s $f ~/$f;
+fi
+done;
+
+vim -es -c "BundleInstall" -c "q"
