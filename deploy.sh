33940432e843266c809331f73bc35c2857abef1b
Jack McCracken <jackamc19@gmail.com>
2014-07-10 15:19:26 -0700

    Stop Vim from flashing up. Need to redirect stderr but I can't figure that out right now.

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,12 @@
 
 command -v zsh >/dev/null 2>&1 || { echo >&2 "This script requires zsh to be installed and on PATH.  Aborting."; exit 1; }
 command -v vim >/dev/null 2>&1 || { echo >&2 "This script requires vim to be installed and on PATH. Aborting."; exit 1; }
+command -v git >/dev/null 2>&1 || { echo >&2 "This script requires vim to be installed and on PATH. Aborting."; exit 1; }
 
-git submodule update --init --recursive;
+echo "Checking Git submodules..."
+git submodule update --init --recursive > /dev/null;
 
+echo "Linking into home directory..."
 for f in .*; 
 do
 if [ "$f" != "." ] && [ "$f" != ".." ] && [ "$f" != ".git" ] && [ "$f" != ".gitmodules" ] && [ ! -e ~/$f ]; then
@@ -12,4 +15,10 @@ if [ "$f" != "." ] && [ "$f" != ".." ] && [ "$f" != ".git" ] && [ "$f" != ".gitm
 fi
 done;
 
-vim -E -c BundleInstall -c qall
+echo "Launching Vim to install bundles..."
+vim -E -c BundleInstall -c qall > /dev/null
+
+echo "Note: Loading Emacs for the first time after deploy may take a little longer due to package install."
+
+echo ""
+echo "Deployment successful!"
