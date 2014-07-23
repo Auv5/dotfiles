a8d64fff8a0ed679d16d8a19f5fe7fcf286b1bc4
Jack McCracken <jackamc19@gmail.com>
2014-07-23 00:46:33 -0400

    Added style for C++

--- .emacs.d/style.el
+++ .emacs.d/style.el
@@ -9,3 +9,8 @@
 			 (setq c-basic-offset 2
 			       tab-width 2
 			       indent-tabs-mode nil)))
+
+(add-hook 'c++-mode-hook (lambda ()
+			   (setq c-basic-offset 4
+				 tab-width 4
+				 indent-tabs-mode t)))
